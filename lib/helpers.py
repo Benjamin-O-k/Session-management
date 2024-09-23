b06a2cc52e1e890d02018d466a2e9029940b1084
Benjamin-O-k <benjamin.okello@student.moringaschool.com>
2024-09-23 07:04:34 +0300

    Update helpers.py

--- lib/helpers.py
+++ lib/helpers.py
@@ -1,84 +1,86 @@
-import sqlalchemy
 from lecturer import Lecturer
 from lesson import Lesson
 from student import Student
 from db import session
-
+import sqlalchemy
 
 # Lesson functions
 def create_lesson():
-    title = str(input("Please enter the title of the class:\n "))
-    location = str(input("Please provide the location of the lesson:\n "))
-    time =str(input("Please provide the time of the class:\n "))
-    lec_id = int(input("Enter the lecture id:\n"))
-    students_id = int(input("Enter student id:\n"))
+    title = input("Please enter the title of the class:\n ")
+    location = input("Please provide the location of the lesson:\n ")
+    time = input("Please provide the time of the class:\n ")
+    lec_id = int(input("Enter the lecturer ID:\n"))
+    student_id = int(input("Enter student ID:\n"))
 
     lecturer = session.query(Lecturer).filter_by(id=lec_id).first()
-    student = session.query(Student).filter_by(id=students_id).first()
+    student = session.query(Student).filter_by(id=student_id).first()
 
     if lecturer and student:
         try:
-            lesson = Lesson(title=title,location=location,time=time,lec_id=lecturer,students_id=student)
+            lesson = Lesson(title=title, location=location, time=time, lec_id=lec_id)
+            lesson.students.append(student)  # Associate student with lesson
             session.add(lesson)
             session.commit()
-
-            print(f"Lesson added successfully!.{lesson}")
+            print(f"Lesson added successfully: {lesson}")
         except Exception as e:
-            print(f"Sorry an error occurred while creating the lesson: {e}")
+            print(f"Error occurred while creating the lesson: {e}")
     else:
-        print(f"Lecturer with ID {lec_id} or Student with ID {students_id} not found.")
+        print(f"Lecturer with ID {lec_id} or Student with ID {student_id} not found.")
 
 def find_lesson_by_name():
-    name = str(input("Enter lesson name you want to search:\n "))
-    lesson = session.query(Lesson).filter(Lesson.title.like('%'+name+'%')).all()
-    if lesson:
-        print(f"Lesson found!: {lesson}")
+    name = input("Enter lesson name you want to search:\n ")
+    lessons = session.query(Lesson).filter(Lesson.title.like(f'%{name}%')).all()
+    if lessons:
+        for lesson in lessons:
+            print(f"Lesson found: {lesson.title}")
     else:
-        print(f"The lesson {name} has not been found.Please try again")
+        print(f"The lesson {name} has not been found. Please try again.")
 
 def all_lessons():
     lessons = session.query(Lesson).all()
-    if lessons :
+    if lessons:
         for lesson in lessons:
-            print(f"Here are the lessons: Title - {lesson.title}, Location - {lesson.location}, Time - {lesson.time}")
+            print(f"Title: {lesson.title}, Location: {lesson.location}, Time: {lesson.time}")
     else:
         print("No lessons found.")
 
-def students_present():#check this
-    students = Lesson.all_students
-    for student in students:
-        print(student)
+def students_present(lesson_id):
+    lesson = session.query(Lesson).get(lesson_id)
+    if lesson:
+        students = lesson.students
+        if students:
+            for student in students:
+                print(student)
+        else:
+            print("No students present in this lesson.")
+    else:
+        print(f"No lesson found with ID {lesson_id}.")
 
 def find_lesson_by_id():
     id_ = int(input("Enter lesson ID: \n"))
-
-    lesson = session.query(Lesson).filter(Lesson.id == id_).first()
+    lesson = session.query(Lesson).get(id_)
     if lesson:
         print(f"Lesson found: Title - {lesson.title}, Location - {lesson.location}, Time - {lesson.time}")
     else:
-        print(f"The lesson with ID {id_} has not been found. Please try again")
+        print(f"The lesson with ID {id_} has not been found. Please try again.")
 
 def update_lesson():
     id_ = int(input("Enter lesson ID to update: \n"))
     lesson = session.query(Lesson).get(id_)
-    if lesson :
+    if lesson:
         try:
-            title = str(input("Enter new title: \n"))
-            location = str(input("Enter new location: \n"))
-            time = str(input("Enter new time: \n"))
-            lesson.title = title
-            lesson.location = location
-            lesson.time = time
+            lesson.title = input("Enter new title: \n")
+            lesson.location = input("Enter new location: \n")
+            lesson.time = input("Enter new time: \n")
             session.commit()
             print("Lesson updated successfully.")
-        except Exception as ion:
-            print("Error occurred while updating the lesson." ,ion)
+        except Exception as e:
+            print("Error occurred while updating the lesson:", e)
     else:
-        print(f"The lesson with ID {id_} has not been found. Please try again")
+        print(f"The lesson with ID {id_} has not been found. Please try again.")
 
 def delete_lesson():
     id_ = int(input("Enter lesson ID to delete: \n"))
-
     lesson = session.query(Lesson).get(id_)
     if lesson:
         try:
@@ -86,21 +88,19 @@ def delete_lesson():
             session.commit()
             print("Lesson deleted successfully.")
         except Exception as e:
-            print("Error occurred while deleting the lesson.: " ,e)
+            print("Error occurred while deleting the lesson:", e)
     else:
-        print(f"The lesson with ID {id_} has not been found. Please try again")
-
+        print(f"The lesson with ID {id_} has not been found. Please try again.")
 
 # Student functions
-
 def create_student():
-    name = str(input("Please enter the name of the student: \n"))
-    unit = str(input("Please provide the unit the student will be taking: \n"))
+    name = input("Please enter the name of the student: \n")
+    unit = input("Please provide the unit the student will be taking: \n")
     try:
-        student = Student(name=name,unit=unit)
+        student = Student(name=name, unit=unit)
         session.add(student)
         session.commit()
-        print(f"Student added successfully.: {student}")
+        print(f"Student added successfully: {student}")
     except Exception as e:
         print(f"Error occurred while adding the student: {e}")
 
@@ -108,9 +108,9 @@ def find_student_by_name():
     name = input("Enter student name: \n")
     student = session.query(Student).filter_by(name=name).first()
     if student:
-        print(f"Student found!: {student}")
+        print(f"Student found: {student}")
     else:
-        print(f"The student named {name} has not been found.Please try again")
+        print(f"The student named {name} has not been found. Please try again.")
 
 def find_student_by_id():
     id_ = int(input("Enter student ID:\n "))
@@ -122,23 +122,20 @@ def find_student_by_id():
 
 def update_student():
     id_ = int(input("Enter student ID to update:\n "))
-    student  = session.query(Student).get(id_)
-    if student :#:= Student.find_by_id(id_):
+    student = session.query(Student).get(id_)
+    if student:
         try:
-            name = str(input("Enter new name:\n "))
-            unit = str(input("Enter new unit:\n "))
-            student.name = name
-            student.unit = unit
+            student.name = input("Enter new name:\n ")
+            student.unit = input("Enter new unit:\n ")
             session.commit()
             print("Student updated successfully.")
-        except Exception as ion:
-            print("Error occurred while updating the student." ,ion)
+        except Exception as e:
+            print("Error occurred while updating the student:", e)
     else:
-        print(f"The student with ID {id_} has not been found. Please check the students ids and try again")
+        print(f"The student with ID {id_} has not been found. Please try again.")
 
 def delete_student():
     id_ = int(input("Enter student ID to delete:\n "))
-
     student = session.query(Student).get(id_)
     if student:
         try:
@@ -146,32 +143,31 @@ def delete_student():
             session.commit()
             print("Student deleted successfully.")
         except Exception as e:
-            print("Error occurred while deleting the student.Please try again later or contact the admin." ,e)
+            print("Error occurred while deleting the student:", e)
     else:
-        print(f"The student with ID {id_} has not been found. Please check the list of students and try again")
+        print(f"The student with ID {id_} has not been found. Please try again.")
 
 def stud_lecturer():
-    print("All students and their lecturers")
-    students = Student.all_lecturers()#check this
+    print("All students and their lecturers:")
+    students = session.query(Student).all()
     for student in students:
-        print(f"{student.name}: {student.lecturer}")
+        print(f"{student.name}: {student.lecturer.name if student.lecturer else 'No lecturer assigned'}")
 
 def stud_class():
     print("All students and their lessons:")
-    students = Student.all_classes()#check this
+    students = session.query(Student).all()
     for student in students:
-        print(f"{student.name}: {student.lesson}")
+        print(f"{student.name}: {[lesson.title for lesson in student.lessons]}")
 
-
-# lecturer functions
-def create_lecturer():#modified the error messages
-    name = str(input("Enter the name:\n "))
-    profession = str(input("Enter the profession: \n"))
+# Lecturer functions
+def create_lecturer():
+    name = input("Enter the name:\n ")
+    profession = input("Enter the profession: \n")
     try:
-        lecturer = Lecturer(name=name,profession=profession)
+        lecturer = Lecturer(name=name, profession=profession)
         session.add(lecturer)
         session.commit()
-        print(f"Lecturer added successfully.: {lecturer}")
+        print(f"Lecturer added successfully: {lecturer}")
     except sqlalchemy.exc.IntegrityError as e:
         print(f"Error: Lecturer with the same name and profession already exists. {e}")
     except sqlalchemy.exc.OperationalError as e:
@@ -179,87 +175,68 @@ def create_lecturer():#modified the error messages
     except Exception as e:
         print(f"Error occurred while creating the lecturer: {e}")
 
-def find_lecturer_by_name():#added error message and execption cases
+def find_lecturer_by_name():
     name = input("Enter lecturer name: \n")
     try:
-        lecturer = session.query(Lecturer).filter_by(name=name).all()
-        if lecturer:
-            for lecturer in lecturer:
+        lecturers = session.query(Lecturer).filter_by(name=name).all()
+        if lecturers:
+            for lecturer in lecturers:
                 print(f"Lecturer name: {lecturer.name}, Profession: {lecturer.profession}")
         else:
-            print(f"The lecturer {name} has not been found.Please check the list of the lecturers and try again")
+            print(f"The lecturer {name} has not been found. Please try again.")
     except sqlalchemy.exc.OperationalError as e:
         print(f"Error: Database operational error. {e}")
-    except Exception as e:#check this
+    except Exception as e:
         print(f"Error occurred while finding the lecturer: {e}")
 
 def find_lecturer_by_id():
     id_ = int(input("Enter lecturer ID:\n "))
     lecturer = session.query(Lecturer).filter_by(id=id_).first()
     if lecturer:
-        print(lecturer)
+        print(f"Lecturer found: {lecturer}")
     else:
-        print(f"The lecturer with ID {id_} has not been found. Please check the list of available lecturers and try again")
+        print(f"The lecturer with ID {id_} has not been found. Please try again.")
 
 def update_lecturer():
     id_ = int(input("Enter lecturer ID to update: \n"))
-
-    if lecturer := Lecturer.find_by_id(id_):
+    lecturer = session.query(Lecturer).filter_by(id=id_).first()
+    if lecturer:
         try:
-            name = str(input("Enter new name: \n"))
-            profession = str(input("Enter new profession:\n "))
-            lecturer.name = name
-            lecturer.profession = profession
+            lecturer.name = input("Enter new name: \n")
+            lecturer.profession = input("Enter new profession:\n ")
             session.commit()
-            print("Lecturer has been updated successfully.")
+            print("Lecturer updated successfully.")
         except sqlalchemy.exc.OperationalError as e:
             print(f"Error: Database operational error. {e}")
         except Exception as e:
             print(f"Error occurred while updating the lecturer's information: {e}")
     else:
-        print(f"The lecturer with ID {id_} has not been found. Please check from the list of available lecturers and try again")
+        print(f"The lecturer with ID {id_} has not been found. Please try again.")
 
 def delete_lecturer():
     id_ = int(input("Enter lecturer ID to delete:\n "))
-
-    query = session.query(Lecturer).filter_by(Lecturer).filter_by(id=id_).first()
-    if query:
+    lecturer = session.query(Lecturer).filter_by(id=id_).first()
+    if lecturer:
         try:
-            session.delete(query)
+            session.delete(lecturer)
             session.commit()
             print("Lecturer deleted successfully.")
         except Exception as e:
-            print("Error occurred while deleting the lecturer." ,e)
+            print("Error occurred while deleting the lecturer:", e)
     else:
-        print(f"The lecturer with ID {id_} has not been found. Please check and try again")
+        print(f"The lecturer with ID {id_} has not been found. Please try again.")
 
 def lec_classes():
     print("All lecturers and their classes:")
-    try:
-        lecturers = Lecturer.all_classes()
-        for lecturer in lecturers:
-            try:
-                print(f"{lecturer.name}: {lecturer.lesson}")
-            except AttributeError:
-                print(f"Lecturer {lecturer} does not have a name or lesson.")
-    except AttributeError:
-        print("Lecturer class does not have an all_classes method.")
-    except Exception as e:
-        print("An error occurred:", e)
-def lec_students():#added error messages and exception cases
-    print("All lecturers and their students:")
-    try:
-        lecturers = Lecturer.all_students()
-        for lecturer in lecturers:
-            try:
-                print(f"{lecturer.name}: {lecturer.students}")
-            except AttributeError:
-                print(f"Lecturer {lecturer} does not have a name or students.")
-    except AttributeError:
-        print("Lecturer class does not have an all_students method.")
-    except Exception as e:
-        print("An error occurred:", e)
+    lecturers = session.query(Lecturer).all()
+    for lecturer in lecturers:
+        print(f"{lecturer.name}: {[lesson.title for lesson in lecturer.lessons]}")
 
+def lec_students():
+    print("All lecturers and their students:")
+    lecturers = session.query(Lecturer).all()
+    for lecturer in lecturers:
+        print(f"{lecturer.name}: {[student.name for student in lecturer.students]}")
 
 # Main menu
 def items():
@@ -269,26 +246,26 @@ def items():
         print("2. Find a lesson by name")
         print("3. List all lessons")
         print("4. List all students present in a lesson")
-        print("5. Find a lesson by the id")
+        print("5. Find a lesson by ID")
         print("6. Update a lesson")
         print("7. Delete a lesson")
         print("8. Create a student")
         print("9. Find a student by name")
-        print("10. Find a student by id")
+        print("10. Find a student by ID")
         print("11. Update a student")
         print("12. Delete a student")
-        print("13. List all students and there lecturer")
-        print("14. List all students and there lessons")
+        print("13. List all students and their lecturers")
+        print("14. List all students and their lessons")
         print("15. Create a lecturer")
-        print("16. Find a lecturer by the name")
-        print("17. Find a lecturer by id")
-        print("18. Update a lecturer information")
-        print("19. Delete a lecturer's information")
+        print("16. Find a lecturer by name")
+        print("17. Find a lecturer by ID")
+        print("18. Update a lecturer")
+        print("19. Delete a lecturer")
         print("20. List all lecturers and their classes")
         print("21. List all lecturers and their students")
-        print("23. Exit")
+        print("22. Exit")
 
-        choice = str(input("Enter your choice: "))
+        choice = input("Enter your choice: ")
 
         if choice == '1':
             create_lesson()
@@ -297,10 +274,11 @@ def items():
         elif choice == '3':
             all_lessons()
         elif choice == '4':
-            students_present()
+            lesson_id = int(input("Enter lesson ID to list students: "))
+            students_present(lesson_id)
         elif choice == '5':
             find_lesson_by_id()
-        elif choice =='6':
+        elif choice == '6':
             update_lesson()
         elif choice == '7':
             delete_lesson()
@@ -332,13 +310,13 @@ def items():
             lec_classes()
         elif choice == '21':
             lec_students()
-        elif choice == '23':
+        elif choice == '22':
             exit_program()
         else:
             print("Invalid choice. Please try again.")
 
 def exit_program():
-    x = input("Are you sure you want to exit the program:(y/n)")
+    x = input("Are you sure you want to exit the program? (y/n): ")
     if x.lower() == 'y':
         print("Exiting the program...")
         exit()
